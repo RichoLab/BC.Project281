f39bdd60d50c3b8a698e444270a7a638f4dbfc14
Elaine-09 <elainemeintjes@gmail.com>
2025-08-19 11:59:59 +0200

    Update AdminAuth.cs

--- Project281/AdminAuth.cs
+++ Project281/AdminAuth.cs
@@ -8,7 +8,6 @@ namespace Project281_Ryno_File
 {
     internal class AdminAuth
     {
-        // SIMPLE: plain text password (change if you like)
         private const string Password = "Admin123";
 
         public static bool Verify(string input) => input == Password;
